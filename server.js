821abddd1693963ef801677ccf1ee7bfee848668
Kshitiz-KBen <kshitizsingh004@gmail.com>
2023-10-29 01:38:46 +0530

    added models and redifined all the codes

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require("express")
+const express = require("express");
+const db = require("./models");
 
 const app = express();
 
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 
 app.get("/movies", (req, res) => {
@@ -17,3 +32,11 @@ app.get("/artists", (req, res) => {
 app.listen(9000, () => {
     console.log("Listening on port 9000");
 })
+
+
+const getAllUsers = async() => {
+    const users = await db.users.find({});
+    console.log(users);
+}
+
+getAllUsers();
